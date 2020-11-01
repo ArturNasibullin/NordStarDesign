bd47ba769d951898c49baf6d1039f6b4ac5b2205
Artur Nasibullin <nasibullin.artur@gmail.com>
2020-11-01 19:15:57 +0500

    добавил функционал

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,54 @@
 $(function() {
+  let header = $('#header');
+  let mainPage = $('#mainPage');
+  let mainPageH = mainPage.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  let burgerBtn = $('#burgerToggle');
+  let menu = $('#headerMenu');
+
+  $(window).on('scroll load resize', function() {
+     let mainPageH = mainPage.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    if (scrollPos > mainPageH) {
+      header.addClass('fixed');
+    } else {
+      header.removeClass('fixed');
+    }
+  });
+
+  // Smooth scroll
+  $('[data-scroll]').on('click', function(event){
+    event.preventDefault();
+
+    let elementID = $(this).data('scroll');
+    let elementOffset = $(elementID).offset().top;
+    
+
+    $('html, body').animate({
+      scrollTop: elementOffset - 80
+    }, 1000);
+
+
+    menu.removeClass('show');
+    
+  });
+  
+  // Burger
+  burgerBtn.on('click', function() {
+
+    menu.toggleClass('show');
+
+  });
+
+
+});
+
+
+
+
+// slider
+$(function() {
   let slider = $('#galerySlider'),
       secondSlider = $('#processCorusele');
 
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,76 @@
 $(function() {
-  let slider = $('#galerySlider');
+  let header = $('#header');
+  let mainPage = $('#mainPage');
+  let mainPageH = mainPage.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  let burgerBtn = $('#burgerToggle');
+  let menu = $('#headerMenu');
 
-  slider.slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    dots: true,
-    prevArrow: $('.arrow__prev'),
-    nextArrow: $('.arrow__next'),
+  $(window).on('scroll load resize', function() {
+     let mainPageH = mainPage.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    if (scrollPos > mainPageH) {
+      header.addClass('fixed');
+    } else {
+      header.removeClass('fixed');
+    }
+  });
+
+  // Smooth scroll
+  $('[data-scroll]').on('click', function(event){
+    event.preventDefault();
+
+    let elementID = $(this).data('scroll');
+    let elementOffset = $(elementID).offset().top;
+    
+
+    $('html, body').animate({
+      scrollTop: elementOffset - 80
+    }, 1000);
+
+
+    menu.removeClass('show');
+    
+  });
+  
+  // Burger
+  burgerBtn.on('click', function() {
+
+    menu.toggleClass('show');
 
   });
 
+
+});
+
+
+
+
+// slider
+$(function() {
+  let slider = $('#galerySlider'),
+      secondSlider = $('#processCorusele');
+
+      slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+        prevArrow: $('.arrow__prev'),
+        nextArrow: $('.arrow__next'),
+
+      });
+
+      secondSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      });
 });
